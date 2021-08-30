6873cd8a42f3433b623b1d77171367e7e42a914f
MohamedHasan <muhamedmustafa3.14@gmail.com>
2021-08-30 02:10:44 +0200

    add drivers page table

--- lib/pages/drivers/drivers.dart
+++ lib/pages/drivers/drivers.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:web_dashboard/constants/controllers.dart';
+import 'package:web_dashboard/helpers/responsiveness.dart';
+import 'package:web_dashboard/pages/drivers/widgets/drivers_table.dart';
+import 'package:web_dashboard/widgets/custom_text.dart';
 
 class DriversPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Drivers '),
+    return Column(
+      children: [
+        Obx(() => Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                      top: Responsiveness.isSmallScreen(context) ? 56 : 6),
+                  child: CustomText(
+                    text: menuController.activeItem.value,
+                    size: 24,
+                    weight: FontWeight.bold,
+                  ),
+                )
+              ],
+            )),
+        Expanded(
+          child: ListView(
+            children: [DriversTable()],
+          ),
+        )
+      ],
     );
   }
 }
